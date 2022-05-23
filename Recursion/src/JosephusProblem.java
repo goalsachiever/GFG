e49bbfa05246bb38d1abcef6f4963ebb711b9eda
goalsachiever <goalsachiever8@gmail.com>
2022-05-23 05:45:19 +0530

    Josephus problem

---
+++ Recursion/src/JosephusProblem.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class JosephusProblem
+{
+    //Starting from 0 to n-1
+    static int jos(int n, int k )
+    {
+        if(n==1)
+            return  0;
+        return (jos(n-1,k)+k)%n;
+    }
+    //Starting from 1 to n
+    static int myJos(int n, int k)
+    {
+        return jos(n, k) + 1;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        myJos(n,k);
+    }
+}
