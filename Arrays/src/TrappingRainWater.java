90d2060706533fdac255eaf8aa12c4b58183f369
goalsachiever <goalsachiever8@gmail.com>
2022-05-27 22:31:23 +0530

    trapping rain water problem

--- Arrays/src/TrappingRainWater.java
+++ Arrays/src/TrappingRainWater.java
@@ -3,24 +3,17 @@ import java.util.Scanner;
 public class TrappingRainWater
 {
     static int getWater(int[] arr)
-    {
-        int[] left = new int[arr.length];
+    {   int[] left = new int[arr.length];
         int[] right = new int[arr.length];
         left[0]=arr[0];
         for(int i=1;i< arr.length;i++)
-        {
             left[i]=Math.max(arr[i],left[i-1]);
-        }
         right[arr.length-1]=arr[arr.length-1];
         for(int i= arr.length-2;i>=0;i--)
-        {
             right[i]=Math.max(arr[i],right[i+1]);
-        }
         int water =0;
         for(int i=1;i<arr.length-1;i++)
-        {
             water = water + Math.min(left[i],right[i])-arr[i];
-        }
         return water;
     }
     public static void main(String[] args)
